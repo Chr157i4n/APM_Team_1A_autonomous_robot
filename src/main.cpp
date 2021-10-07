bf4452afb766e3ae91a399edd819aa00dd5833fd
Christian Köhlke <christian@koehlke.de>
2021-10-07 14:39:04 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Neustart");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED AN");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED AUS");
+  delay(1000);
 }
