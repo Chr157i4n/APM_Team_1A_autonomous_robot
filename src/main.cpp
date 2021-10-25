94e4cef5ee5541daed141c5132b04141cc466f05
Christian Köhlke <christian@koehlke.de>
2021-10-25 11:13:36 +0200

    driving time measurement - the bot start driving after DURATION_INITIAL_WAIT ms - the bot drives for DURATION_DRIVE ms - a little bit Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,23 @@
 #include "TB6612MotorShield.h"
 #include "LineSensor.h"
 
-#define PIN_LINESENSOR A7
+#define BAUD_RATE 9600
+#define PIN_LINESENSOR_SENSE A7
+#define PIN_LINESENSOR_POWER 12
+#define DURATION_INITIAL_WAIT 5000 //ms
+#define DURATION_DRIVE 20000 //ms
+
 
 TB6612MotorShield motor;
-LineSensor lineSensor(PIN_LINESENSOR);
+LineSensor lineSensor(PIN_LINESENSOR_SENSE);
 
+unsigned int timeStart = 0, timeCurrent = 0;
 
+//variables for driving
 int baseSpeed = 200;
-double Setpoint = 512, lineSensorValue, lineSensorPIDValue;
+double Setpoint = 512, lineSensorValue = 0, lineSensorPIDValue = 0;
 
-//Specify the links and initial tuning parameters
+// PID for steering
 double Kp=0.1, Ki=0, Kd=0;
 PID lineSensorPID(&lineSensorValue, &lineSensorPIDValue, &Setpoint, Kp, Ki, Kd, DIRECT);
 
@@ -29,18 +36,21 @@ void setup() {
   lineSensorPID.SetOutputLimits(-100,100);
   lineSensorPID.SetMode(AUTOMATIC);
 
-  Serial.begin(9600);
-  pinMode(12,OUTPUT);
-  digitalWrite(12,HIGH);
+  Serial.begin(BAUD_RATE);
+  pinMode(PIN_LINESENSOR_POWER, OUTPUT);
+  digitalWrite(PIN_LINESENSOR_POWER, HIGH);
 
   motor.setSpeeds(0,0);           //Ruckbewegung der Motoren am Anfang fällt hiermit weg.
-  delay(1000);
+  delay(DURATION_INITIAL_WAIT);
 
-
+  timeStart = millis();
 }
 
 void loop() {
   
+  timeCurrent = millis();
+
+  if(timeCurrent < timeStart + DURATION_DRIVE){
   lineSensorValue = lineSensor.getValue();
   //normalizedsensorValue = (sensorValue - 512) * 0.1;
 
@@ -58,6 +68,11 @@ void loop() {
   Serial.print(" rS: ");
   Serial.println(baseSpeed-lineSensorPIDValue);
 
-  setMotorSpeeds(baseSpeed+lineSensorPIDValue, baseSpeed-lineSensorPIDValue); 
+  setMotorSpeeds(baseSpeed+lineSensorPIDValue, baseSpeed-lineSensorPIDValue);
+
+  } else {
+    setMotorSpeeds(0, 0);
+    Serial.println("Destination reached");
+  }
 
 }
