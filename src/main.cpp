f5b19d923658609e7df9620bc2f16a9c8cd793a2
Christian Köhlke <christian@koehlke.de>
2021-10-27 18:32:48 +0200

    testing in university

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #define BAUD_RATE 9600
 #define PIN_LINESENSOR_SENSE A7
 #define PIN_LINESENSOR_POWER 12
-#define DURATION_INITIAL_WAIT 5000 //ms
+#define DURATION_INITIAL_WAIT 1000 //ms
 #define DURATION_DRIVE 20000 //ms
 
 
@@ -16,12 +16,14 @@ LineSensor lineSensor(PIN_LINESENSOR_SENSE);
 
 unsigned long timeStart = 0, timeCurrent = 0;
 
+double lineSensorMax = 560, lineSensorMin = 450;
+
 //variables for driving
-int baseSpeed = 200;
+int baseSpeed = 100;
 double Setpoint = 512, lineSensorValue = 0, lineSensorPIDValue = 0;
 
 // PID for steering
-double Kp=0.1, Ki=0, Kd=0;
+double Kp=0.6, Ki=0.1, Kd=0.2;
 PID lineSensorPID(&lineSensorValue, &lineSensorPIDValue, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 
@@ -31,7 +33,7 @@ PID lineSensorPID(&lineSensorValue, &lineSensorPIDValue, &Setpoint, Kp, Ki, Kd, 
 */
 void setMotorSpeeds(int m1Speed, int m2Speed){
   
-  motor.setSpeeds(m1Speed, -m2Speed);
+  motor.setSpeeds(-m1Speed, m2Speed);
 
 }
 
@@ -42,10 +44,12 @@ void setMotorSpeeds(int m1Speed, int m2Speed){
 */
 void setup() {
 
+  Setpoint = (lineSensorMax + lineSensorMin)*0.5;
+
   motor.setSpeeds(0,0);                         // Ruckbewegung der Motoren am Anfang fällt hiermit weg.
   motor.setBreak(true);
 
-  lineSensorPID.SetOutputLimits(-100,100);      // standard of the limits is (0, 255) and we need negative values
+  lineSensorPID.SetOutputLimits(-50,50);      // standard of the limits is (0, 255) and we need negative values
   lineSensorPID.SetMode(AUTOMATIC);
 
   Serial.begin(BAUD_RATE);
@@ -66,14 +70,14 @@ void setup() {
 void loop() {
   
   timeCurrent = millis();                                     // get the current time
-
   if(timeCurrent < timeStart + DURATION_DRIVE){               // only drive for a defined amount of time
     
     lineSensorValue = lineSensor.getValue();                    // reading the line sensor (phototransistor) value
     //normalizedsensorValue = (sensorValue - 512) * 0.1;
 
     lineSensorPID.Compute();                                    // compute the output value for the steering based on the line sensor value (part of the PID libary)
-
+    Serial.print(" set: ");
+    Serial.print(Setpoint);
     Serial.print(" raw: ");
     Serial.print(lineSensorValue);
     Serial.print(" pid: ");
@@ -90,7 +94,22 @@ void loop() {
     
     setMotorSpeeds(0, 0);
     motor.setBreak(true);
-    Serial.println("Destination reached");
+    //Serial.println("Destination reached");
+
+    lineSensorValue = lineSensor.getValue();                    // reading the line sensor (phototransistor) value
+    //normalizedsensorValue = (sensorValue - 512) * 0.1;
+
+    lineSensorPID.Compute();                                    // compute the output value for the steering based on the line sensor value (part of the PID libary)
+    Serial.print(" set: ");
+    Serial.print(Setpoint);
+    Serial.print(" raw: ");
+    Serial.print(lineSensorValue);
+    Serial.print(" pid: ");
+    Serial.print(lineSensorPIDValue);
+    Serial.print(" lS: ");
+    Serial.print(baseSpeed+lineSensorPIDValue);
+    Serial.print(" rS: ");
+    Serial.println(baseSpeed-lineSensorPIDValue);
     
   }
 
