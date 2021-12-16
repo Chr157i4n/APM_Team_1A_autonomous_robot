4d94a41c0362e8b83ce3e3c2acd41227159c7e18
Christian Köhlke <christian@koehlke.de>
2021-12-16 10:16:02 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,11 @@
 #define PRINT_DEBUG 1
 
 
-TB6612MotorShield motor;
-FotoTransistorSensor lineSensor(PIN_LINESENSOR_SENSE);
-FotoTransistorSensor batonSensor(PIN_BATONSENSOR_SENSE);
+//TB6612MotorShield motor;
+//FotoTransistorSensor lineSensor(PIN_LINESENSOR_SENSE);
+//FotoTransistorSensor batonSensor(PIN_BATONSENSOR_SENSE);
 Ultrasonic ultrasonic(PIN_ULTRASONIC_SENSOR_TRIGGER, PIN_ULTRASONIC_SENSOR_ECHO);
-BatonMechanism batonMechanism(PIN_SERVO);
+//BatonMechanism batonMechanism(PIN_SERVO);
 
 unsigned long timeStart = 0, timeCurrent = 0, timeElasped = 0;
 int distanceSecondRobot = 0, batonSensorValue = 0;
@@ -73,7 +73,7 @@ PID lineSensorPID(&lineSensorValue, &lineSensorPIDValue, &Setpoint, Kp, Ki, Kd, 
 */
 void setMotorSpeeds(int m1Speed, int m2Speed){
   
-  motor.setSpeeds(-m1Speed, m2Speed);
+  //motor.setSpeeds(-m1Speed, m2Speed);
 
 }
 
@@ -92,17 +92,17 @@ void setup() {
   // we still need to calibrate this 
   Setpoint = (lineSensorMax + lineSensorMin)*0.5;
 
-  motor.setSpeeds(0,0);                         // Ruckbewegung der Motoren am Anfang fällt hiermit weg.
-  motor.setBreak(true);
+  //motor.setSpeeds(0,0);                         // Ruckbewegung der Motoren am Anfang fällt hiermit weg.
+  //motor.setBreak(true);
 
   lineSensorPID.SetOutputLimits(-50,50);      // standard of the limits is (0, 255) and we need negative values
   lineSensorPID.SetMode(AUTOMATIC);
 
-  batonMechanism.init();
+  //batonMechanism.init();
 
   delay(DURATION_INITIAL_WAIT);                 // Wait a couple of seconds to start
 
-  motor.setBreak(false);
+  //motor.setBreak(false);
   timeStart = millis();                         // save the current time as starting time
 
   Serial.println("setup finished");
@@ -115,64 +115,7 @@ void setup() {
 */
 void loop() {
 
-  timeCurrent = millis(); // get the current time
-  timeElasped = timeCurrent - timeStart;
-
-  if(state == 0){
-    batonSensorValue = batonSensor.getValue(); //todo: change to second sensor
-#if PRINT_DEBUG == 1
-    Serial.println((String)"batonSensorValue: "+batonSensorValue);
-#endif
-
-    if(batonSensorValue < THRESHOLD_BATONSENSOR_DETECT){ 
-      //if the brightness is below the THRESHOLD_BATONSENSOR_DETECT the robot goes into state 1
-      state = 1;
-      Serial.println("robot should start driving in 2 seconds");
-      delay(DURATION_INITIAL_WAIT_AFTER_BATON_DETECTED);
-      timeStart = timeCurrent;
-    }
-
-  } else if(state == 1){
-    distanceSecondRobot = ultrasonic.read();
-    Serial.println("Distance: "+distanceSecondRobot);
-
-    if(distanceSecondRobot < 20 || timeElasped > DURATION_DRIVE_TIMEOUT){
-      // if the measured distance of the ultrasonic sensor is below 20cm the robot should go into state 2 (stop)
-      state = 2;
-      Serial.println("destination or timeout reached");
-    } 
-
-    lineSensorValue = lineSensor.getValue();  // reading the line sensor (phototransistor) value
-    //normalizedsensorValue = (sensorValue - 512) * 0.1;
-
-    lineSensorPID.Compute();  // compute the output value for the steering based on the line sensor value (part of the PID libary)
-
-#if PRINT_DEBUG == 1
-    Serial.println((String)"set: "+Setpoint+" raw: "+lineSensorValue+" pid: "+lineSensorPIDValue+" lS: "+(baseSpeed+lineSensorPIDValue)+" rS: "+(baseSpeed-lineSensorPIDValue));
-#endif
-
-    // if you comment this out, the motor does not start to move
-    setMotorSpeeds(baseSpeed+lineSensorPIDValue, baseSpeed-lineSensorPIDValue); // set the actual motor speed
-
-  } else if(state == 2) {
-    //second robot reached
-
-    setMotorSpeeds(0, 0);
-    motor.setBreak(true);
-    batonMechanism.unload();
-    state = 3;
-
-  } else {
-
-#if PRINT_DEBUG == 1
-    lineSensorValue = lineSensor.getValue();                    // reading the line sensor (phototransistor) value
-
-    lineSensorPID.Compute();                                    // compute the output value for the steering based on the line sensor value (part of the PID libary)
-
-    //still outputing the pid values after finished driving for testing purposes
-    Serial.println((String)"set: "+Setpoint+" raw: "+lineSensorValue+" pid: "+lineSensorPIDValue+" lS: "+(baseSpeed+lineSensorPIDValue)+" rS: "+(baseSpeed-lineSensorPIDValue));
-#endif
-
-  }
+  distanceSecondRobot = ultrasonic.read();
+  Serial.println(distanceSecondRobot);
 
 }
